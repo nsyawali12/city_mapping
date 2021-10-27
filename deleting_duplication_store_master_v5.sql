9da3926079b7daec033de062d7b69e691b006cda
unknown <nsyawali12@gmail.com>
2021-10-27 16:26:03 +0700

    deleting duplicate

--- deleting_duplication_store_master_v5.sql
+++ deleting_duplication_store_master_v5.sql
@@ -2,9 +2,12 @@
 -- delete from `telkomsel-retail-intelligence.telkomsel_retail_intelligence.store_master_v5`
 -- where store_city is null and store_province is null;
 
+--- DELETING ONLY one store id
+
 delete from `telkomsel-retail-intelligence.telkomsel_retail_intelligence.store_master_v5`
 where storeid = '1201001884' and updatedAt != '2021-05-28 09:35:46.808 UTC';
 
+--- DELETING DUPLICATE BY AREA
 delete from `telkomsel-retail-intelligence.telkomsel_retail_intelligence.store_master_v5`
 where store_area = 'AREA 1' and id not in (select * from (select min (id)
 from `telkomsel-retail-intelligence.telkomsel_retail_intelligence.store_master_v5`
